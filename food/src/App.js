61fa89cce5a02b104905f0586fe04da1fe12f70d
Grace Chen <grace.chenxinyue@berkeley.edu>
2021-01-09 16:47:46 -0800

    added signin component

--- food/src/App.js
+++ food/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Signin from './components/Signin/Signin';
+// import Register from './components/Register/Register';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    // const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <div className="center">
+          <Signin 
+          // loadUser={this.loadUser} onRouteChange={this.onRouteChange}
+          />
+        </div>
+        <footer class = "sticky">
+        
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
