dc1bfaabf4f3a23da700c4ab145c187049b0b233
Grace Chen <grace.chenxinyue@berkeley.edu>
2021-01-09 17:08:34 -0800

    sign up page component

--- food/src/App.js
+++ food/src/App.js
@@ -4,34 +4,37 @@ import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
 
-function App() {
+class App extends Component {
+  // constructor() {
+  // var config = {
+  //   apiKey: " AIzaSyBsQhiBeBxUxQw744BHlQfEPty5aUhbLY4 ",
+  //   authDomain: "n-tri-569d8.firebaseapp.com",
+  //   databaseURL: "https://n-tri-569d8-default-rtdb.firebaseio.com/",
+  //   storageBucket: "gs://n-tri-569d8.appspot.com/"
+  // };
+  // firebase.initializeApp(config);
 
-  var config = {
-    apiKey: " AIzaSyBsQhiBeBxUxQw744BHlQfEPty5aUhbLY4 ",
-    authDomain: "n-tri-569d8.firebaseapp.com",
-    databaseURL: "https://n-tri-569d8-default-rtdb.firebaseio.com/",
-    storageBucket: "gs://n-tri-569d8.appspot.com/"
-  };
-  firebase.initializeApp(config);
-
-  // Get a reference to the database service
-  var database = firebase.database();
-
+  // // Get a reference to the database service
+  // var database = firebase.database();
+  // }
+  
   // Set the configuration for your app
   // TODO: Replace with your project's config object
 
-  return (
-    <div className="App">
-      <div className="center">
-        <Signin 
-        // loadUser={this.loadUser} onRouteChange={this.onRouteChange}
-        />
+  render () {
+    return (
+      <div className="App">
+        <div className="center">
+          <Signin 
+          // loadUser={this.loadUser} onRouteChange={this.onRouteChange}
+          />
+        </div>
+        <footer class = "sticky">
+      
+        </footer>
       </div>
-      <footer class = "sticky">
-    
-      </footer>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
